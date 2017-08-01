789d0ae42b6d717d7f7d8a79f671b00e026c05cd
Admin <admin@Willis-MacBook-Pro.local>
2017-07-31 21:02:08 -0400

    updated the build

--- app/src/pages/about.js
+++ app/src/pages/about.js
@@ -15,7 +15,7 @@ const styles = {
 };
 
 const Img1 ={
-    padding:"20",
+    padding:"28",
     width: "100%",
     height: "100%",
 };
@@ -55,19 +55,24 @@ const About = () => {
             <div className="about" style={styles}>
             <Grid>
                     <Col lg={6} >
+                        <p style={Content}>We’re parents, just like you. We recently had a baby and we quickly came to this realization:
+                             The problem with baby stuff isn’t that you don’t know what to get.  You know exactly what you need, but there are so many choices.
+                             For everything. Hundreds of bottles. Just as many nipples. Bibs. Pacifiers. Countless car seats.
+                             Strollers that range in price from $50 to thousands. Diapers galore. Bouncers, swings, and chairs that’ll make your head spin.
+                             Toys of all types. Cribs. Bassinets. Co-sleepers. And that’s just the start! The options seem endless, so we’re here to help.</p>
 
-                <p style={Content}>We’re parents, just like you. We recently had a baby and we quickly came to this realization:
-                    The problem with baby stuff isn’t that you don’t know what to get.  You know exactly what you need, but there are so many choices. For everything. Hundreds of bottles. Just as many nipples. Bibs. Pacifiers. Countless car seats. Strollers that range in price from $50 to thousands. Diapers galore. Bouncers, swings, and chairs that’ll make your head spin. Toys of all types. Cribs. Bassinets. Co-sleepers. And that’s just the start! The options seem endless, so we’re here to help.
-                    The Well-Stocked Stork is our brainchild. We’re Willis and Darcy, a real-life couple with a newborn son who was born the summer of 2017, more than nine years after his older sister arrived on the scene. Boy oh boy, have things changed in a decade!
-                    We’ve combed through product reviews, ratings and old-fashioned recommendations from actual moms and dads to bring you the best products from a wide-array of retailers. The best part? We’ll tailor our suggestions to match your personality, budget and style.
-                    When you use our recommendations, we’re confident that The Well-Stocked Stork will help you make the best selections to suit your family.
-                    Here’s to your next special delivery,
+                        <p style={Content}>The Well-Stocked Stork is our brainchild. We’re Willis and Darcy, a real-life couple with a newborn son who was born the summer of 2017, more than nine years after his older sister arrived on the scene.
+                             Boy oh boy, have things changed in a decade! We’ve combed through product reviews, ratings and old-fashioned recommendations from actual moms and dads to bring you the best products from a wide-array of retailers.
+                             The best part? We’ll tailor our suggestions to match your personality, budget and style.</p>
 
-                    Willis and Darcy
+                        <p style={Content}> When you use our recommendations, we’re confident that The Well-Stocked Stork will help you make the best selections to suit your family.
+                            Here’s to your next special delivery,</p>
+
+                        <p style={Content}> Willis and Darcy
                 </p>
                     </Col>
                     <Col  lg={6} >
-                        <p></p>
+
                         <img style={Img1} src={logo}/>
                     </Col>
 
